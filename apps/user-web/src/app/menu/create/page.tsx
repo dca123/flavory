017e17690b6416a3e312413123e29c85f12b16ce
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-06-15 21:09:38 +0800

    created form

---
+++ apps/user-web/src/app/menu/create/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const Schema = z.object({
+  name: z.string().min(3).max(255),
+  description: z.string().min(3).max(255),
+  price: z.number().min(0.01).max(999.99),
+});
+type Schema = z.infer<typeof Schema>;
+
+export default function Home() {
+  const form = useForm<Schema>({
+    resolver: zodResolver(Schema),
+    defaultValues: {
+      name: "",
+      description: "",
+      price: "" as unknown as number,
+    },
+  });
+  const onSubmit = (data: Schema) => {
+    console.log(data);
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 space-y-16">
+      <h1 className="text-6xl font-bold text-center">Add Item to Menu</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Food Item</FormLabel>
+                <FormControl>
+                  <Input placeholder="Your delicious food name" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This will be the name of your food item
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Your delicious food description"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This will be the description of your food item
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Your delicious food price"
+                    {...field}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This will be the price of your food item
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </main>
+  );
+}

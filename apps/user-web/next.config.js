3e7ceb3d549e56afe8dddd976c5cdf8d974ff9f4
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-11-06 01:06:55 +0800

    upgraded all to latest

--- apps/user-web/next.config.js
+++ apps/user-web/next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   experimental: {
     typedRoutes: true,
-    serverActions: true,
-    swcPlugins: ["@swc-jotai/react-refresh", {}],
+    swcPlugins: [["@swc-jotai/react-refresh", {}]],
   },
 };
 
